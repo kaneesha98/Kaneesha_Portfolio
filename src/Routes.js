994bf4f0d4416edc44e3ee8a72aa23c13bbdf991
Kaneesha Gandhi <kaneesha98@gmail.com>
2021-06-01 09:00:34 +0530

    10

--- src/Routes.js
+++ src/Routes.js
@@ -9,20 +9,23 @@ import P4 from './components/P4';
 import SV from './components/SV';
 import VC from './components/VC';
 import VCP from './components/VCP';
+import {useRoutes} from 'hookrouter';
+
+const routes = {
+    '/Kaneesha_Portfolio/': () => <Home/>,
+    '/Kaneesha_Portfolio/Kaneesha_Portfolio_GEP': () => <GEP />,
+    '/Kaneesha_Portfolio/Kaneesha_Portfolio_VC': () => <VC />,
+    '/Kaneesha_Portfolio/Kaneesha_Portfolio_SV': () => <SV />,
+    '/Kaneesha_Portfolio/Kaneesha_Portfolio_Project1': () => <P1 />,
+    '/Kaneesha_Portfolio/Kaneesha_Portfolio_Project2': () => <P2 />,
+    '/Kaneesha_Portfolio/Kaneesha_Portfolio_Project3': () => <P3 />,
+    '/Kaneesha_Portfolio/Kaneesha_Portfolio_Project4': () => <P4 />,
+    '/Kaneesha_Portfolio/Kaneesha_Portfolio_VC_Project': () => <VCP />,
+};
 
 const Routes=()=>{
-    return(<HashRouter>
-        <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/Kaneesha_Portfolio_GEP" exact component={GEP} />
-            <Route path="/Kaneesha_Portfolio_VC" exact component={VC} />
-            <Route path="/Kaneesha_Portfolio_SV" exact component={SV} />
-            <Route path="/Kaneesha_Portfolio_Project1" exact component={P1} />
-            <Route path="/Kaneesha_Portfolio_Project2" exact component={P2} />
-            <Route path="/Kaneesha_Portfolio_Project3" exact component={P3} />
-            <Route path="/Kaneesha_Portfolio_Project4" exact component={P4} />
-            <Route path="/Kaneesha_Portfolio_VC_Project" exact component={VCP} />
-        </Switch>
-    </HashRouter>)
+    const routeResult = useRoutes(routes);
+    
+    return routeResult;
 }
 export default Routes;
